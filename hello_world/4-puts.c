9fed4e2bef6e10e07785f7a6764e9c59ffce6132
RanimZaouga <ranim.zaougaa@gmail.com>
2023-06-15 15:18:51 -0700

    hello puts

---
+++ hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with put functions 
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{ 
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}
