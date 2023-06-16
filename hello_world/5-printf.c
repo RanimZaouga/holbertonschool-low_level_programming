57b00e7e8c3ca9b112548d2080c5c0c7e5622952
RanimZaouga <ranim.zaougaa@gmail.com>
2023-06-16 04:21:05 -0700

    size

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -12,4 +12,19 @@ int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with printf function.
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
+}
 }
