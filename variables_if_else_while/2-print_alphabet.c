4040674876f2008cf071809e30430affdb73d738
RanimZaouga <ranim.zaougaa@gmail.com>
2023-06-20 13:27:13 -0700

    alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void)
+{
+/*Print the alphabet in lowercase using putchar*/
+for (char letter = 'a'; letter <= 'z'; letter++)
+{
+putchar(letter);
+}
+
+/*Print a new line using putchar*/
+putchar('\n');
+
+return (0);
+}
+
