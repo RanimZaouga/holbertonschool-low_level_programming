830a9b16b0ea247f87136b2365bf9feb0190b2c8
RanimZaouga <ranim.zaougaa@gmail.com>
2023-06-20 13:56:43 -0700

    alphABET

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+char c;
+char C;
+
+c = 'a';
+C = 'A';
+while
+(c <= 'z') (C <= 'Z'); {
+putchar(c);
+putchar(C);
+c++;
+}
+putchar('\n');
+return (0);
+}
