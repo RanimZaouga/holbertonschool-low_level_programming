2a2c1c4608778717b047e6d6fe01747a9e3b121c
RanimZaouga <ranim.zaougaa@gmail.com>
2023-06-21 01:37:55 -0700

    reverse

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: print alphabets backwards
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+char n = 'z';
+
+while
+(n >= 'a') {
+putchar(n);
+n--;
+}
+putchar('\n');
+return (0);
+}
