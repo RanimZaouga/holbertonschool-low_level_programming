644a0871aa47e89ae8fb92eccc1b7fe9c3d2fa43
RanimZaouga <135613007+RanimZaouga@users.noreply.github.com>
2023-06-21 17:56:02 +0000

    alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* main - Entry points
+* Description: utilizes the _putchar function to print the alphabet
+* Return: Always 0 (success)
+*/
+void print_alphabet(void)
+{
+int ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+_putchar(ch);
+_putchar('\n');
+}
