720832c0eb459c163745008809668f1868c27ffe
RanimZaouga <135613007+RanimZaouga@users.noreply.github.com>
2023-06-22 17:38:48 +0000

    98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
 * print_to_98 - entry point
 * @n: compteur
@@ -6,20 +7,13 @@
 */
 void print_to_98(int n)
 {
+int i;
+
 if (n > 98)
-{
-n = n - 1;
-}
-else if (n < 98)
-{
-n = n + 1;
-}
+for (i = n; i > 98; i--)
+printf("%d, ", i);
 else
-{
-n = n;
-}
-_putchar(n + '0');
-_putchar(',');
-_putchar(' ');
-_putchar('\n');
+for (i = n; i < 98; i++)
+printf("%d, ", i);
+printf("98\n");
 }
