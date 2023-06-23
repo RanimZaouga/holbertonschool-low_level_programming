c9150e63fca5dd869c0a7765ba21089dcc82d833
RanimZaouga <135613007+RanimZaouga@users.noreply.github.com>
2023-06-23 10:54:30 +0000

    diagonal

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_diagonal - function that prints a diagonal
+ *
+ * @n: number
+ *
+ * Description: using _putchar to print a diagonal
+ *
+ * Return: Always 0 (success)
+ */
+void print_diagonal(int n)
+{
+int i, s;
+for (i = 1; i <= n; i++)
+{
+_putchar(' ');
+_putchar('\\');
+}
+_putchar('\n');
+}
