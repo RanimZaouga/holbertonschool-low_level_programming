c26891b58fbb70fe43a8a4125c10d3b21d26a19f
RanimZaouga <135613007+RanimZaouga@users.noreply.github.com>
2023-06-23 10:36:08 +0000

    line

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - function that prints a line
+ *
+ * Description: using _putchar to print a line
+ *
+ * Return: Always 0 (success)
+ */
+void print_line(int n)
+{
+for (n = 1; n <= n; n++)
+{
+_putchar('_');
+}
+_putchar('\\');
+_putchar('n');
+
+}
