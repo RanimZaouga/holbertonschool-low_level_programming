1e5bd7f7d8c9ab1177d6b668a1e3988ba3e3bbe8
RanimZaouga <135613007+RanimZaouga@users.noreply.github.com>
2023-06-23 11:40:10 +0000

    diagonal

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -12,6 +12,8 @@
 void print_diagonal(int n)
 {
 int i, space;
+if (n > 0)
+{
 for (i = 1; i <= n; i++)
 {
 for (space = 1; space <= i-1; space++)
@@ -19,5 +21,9 @@ _putchar(' ');
 _putchar('\\');
 _putchar('\n');
 }
+}
+else
+{
 _putchar('\n');
+}
 }
