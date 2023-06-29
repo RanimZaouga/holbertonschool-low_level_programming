7f20eb301f562d91a8641878e46c7dcc6836078f
RanimZaouga <135613007+RanimZaouga@users.noreply.github.com>
2023-06-29 16:04:38 +0000

    copy str

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -9,16 +9,13 @@
 
 char *_strcpy(char *dest, char *src)
 {
-int l = 0;
-int i = 0;
-while (*(src + l) != '\0')
-{
-l++;
-}
-for (i < l; i++)
-{
+int i = -1;
+do {
+i++;
 dest[i] = src[i];
-}
-dest[l] = '\0';
+} while (src[i] != '\0');
+{
 return (dest);
+}
+
 }
