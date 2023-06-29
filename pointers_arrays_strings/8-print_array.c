46180cee70c03757f3fc9498c56053798f5419d3
RanimZaouga <135613007+RanimZaouga@users.noreply.github.com>
2023-06-29 11:20:51 +0000

    array elements

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -15,9 +15,10 @@ print_array(int *a, int n)
 int i;
 for (i = 0; i < n; i++)
 {
+if (i != (n - 1))
+printf("%d, ", a[i]);
+else
 printf("%d", a[i]);
-_putchar(',');
-_putchar(' ');
 }
-_putchar('\n');
+printf('\n');
 }
