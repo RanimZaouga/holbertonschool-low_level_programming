5a144ac79ca668ae52188a62f9fd2e08e8dea48c
RanimZaouga <ranim.zaougaa@gmail.com>
2023-06-30 16:59:43 +0100

    concates

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* _strcat - concatenates strings
+*
+*
+* @dest: str
+* @src: str
+* 
+* Return: pointer
+*/
+
+char *_strcat(char *dest, char *src)
+{
+int i, j;
+
+i = 0;
+while (dest[i])
+c++;
+for (j = 0; src[j]; j++)
+dest[i++] = src[j];
+}
