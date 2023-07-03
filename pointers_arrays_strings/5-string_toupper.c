d76a5031f68cf1b0d5496db26e16d4b408135de0
RanimZaouga <ranim.zaougaa@gmail.com>
2023-07-03 11:26:45 +0100

    fix error

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <string.h>
+/**
+* *string_toupper - changes all lowercase letters of a string to uppercase
+*
+* @s: string
+* 
+* Return: Always 0.
+*/
+
+char *string_toupper(char *s)
+{
+	int n = strlen(s);
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (s[i] = [97 .. 122])
+		{
+		s[i] = s[i] - 32;
+		}
+		else
+		{
+		s[i] = s[i];
+		}
+	}
+	return (0);
+}
