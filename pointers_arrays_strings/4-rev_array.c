b09f3a1bb38236d1c2f2c4ae6d89aab970aaa2ce
RanimZaouga <ranim.zaougaa@gmail.com>
2023-07-03 10:50:31 +0100

    fix error

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* reverse_array - reverse an array of ints
+*
+*
+* @a: name of the array
+* @n: number of elements f an array
+*
+* Return: nothing
+*/
+void reverse_array(int *a, int n)
+{
+	int start = 0;
+	int end = n - 1;
+	int i;
+	for (start < end; start++, end--)
+	{
+		i = a[start];
+        a[start] = a[end];
+        a[end] = i;
+	}
+}
