9d53ae86c7b5208ebe582b9bb7f14d4fdfa46b0c
RanimZaouga <ranim.zaougaa@gmail.com>
2023-07-03 09:44:39 +0100

    fix error

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -15,11 +15,13 @@ char *_strncat(char *dest, char *src, int n)
 
 	j = 0;
 	while (dest[j])
+	{
 		j++;
 		for (i = 0; i < n && src[i] != '\0'; i++)
 		{
 			dest[i + j] = src[i];
 		}
 		dest[i + j] = '\0';
+	}
 	return (dest);
 }
