73a4efae2d4a2d1667a4bf6a626c1605e5f5f7cc
RanimZaouga <ranim.zaougaa@gmail.com>
2023-07-04 22:31:46 +0100

    fix error

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -10,27 +10,21 @@ char *cap_string(char *str)
 
 	while (str[i])
 	{
-		while ((str[i] >= 'a' && str[i] <= 'z'))
-		i++;
-		if (str[i - 1] == ' ' ||
-			str[i - 1] == '\t' ||
-			str[i - 1] == '\n' ||
-			str[i - 1] == ',' ||
-			str[i - 1] == ';' ||
-			str[i - 1] == '.' ||
-			str[i - 1] == '!' ||
-			str[i - 1] == '?' ||
-			str[i - 1] == '"' ||
-			str[i - 1] == '(' ||
-			str[i - 1] == ')' ||
-			str[i - 1] == '{' ||
-			str[i - 1] == '}' ||
-			i == 0)
-			{
+		while (str[i] >= 'a' && str[i] <= 'z')
+			i++;
+
+		if (str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' ||
+		    str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' ||
+		    str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' ||
+		    str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' ||
+		    str[i - 1] == '}' || i == 0)
+		{
 			if (str[i] >= 'a' && str[i] <= 'z')
-				str[i] -= 32; 
-			}
-		i++;		
+				str[i] -= 32;
+		}
+
+		i++;
 	}
-	return (str);
+
+	return str;
 }
