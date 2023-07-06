c2f8b47a8b6e137bcebb21f70f4b5eb42e661b62
RanimZaouga <ranim.zaougaa@gmail.com>
2023-07-06 13:06:56 +0100

    recursion

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -18,13 +18,13 @@ int is_prime_number(int n)
 * @i: checker
 * Return: 1 if prime 0 if not
 */
- int check_prime (int n, int i)
- {
- 	if (n <= 1)
-		return (0);
-	if (n % i == 0 && i > 1)
-		return (0);
-	if ((n / i) < i)
-		return (1);
-	return (check_prime(n, i+1));	
- }		
+int check_prime(int n, int i)
+{
+if (n <= 1)
+return (0);
+if (n % i == 0 && i > 1)
+return (0);
+if ((n / i) < i)
+return (1);
+return (check_prime(n, i+1));	
+}
