831312e9ac6678ca79dabf6673ac29bd177b4e42
RanimZaouga <ranim.zaougaa@gmail.com>
2023-07-06 12:42:00 +0100

    recursion

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -7,16 +7,16 @@
 */
 int is_prime_recursive(int n, int i)
 {
-    if (n <= 1)
-        return (0);
-    
-    if (i == 1)
-        return (1);
-    
-    if (n % i == 0)
-        return (0);
-    
-    return is_prime_recursive(n, i - 1);
+	if (n <= 1)
+		return (0);
+	
+	if (i == 1)
+		return (1);
+	
+	if (n % i == 0)
+		return (0);
+	
+	return is_prime_recursive(n, i - 1);
 }
 
 int is_prime_number(int n)
@@ -26,5 +26,5 @@ int is_prime_number(int n)
 * Return: 1 if primr number
 */
 {
-    return (is_prime_recursive(n, n - 1));
+	return (is_prime_recursive(n, n - 1));
 }
