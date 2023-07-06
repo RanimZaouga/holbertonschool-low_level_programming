f12d34ed6ea0451bf454df09129dad7c8158e479
RanimZaouga <ranim.zaougaa@gmail.com>
2023-07-06 01:35:12 +0100

    fix error

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+* _strpbrk - searches a string for any of a set of bytes
+* @s: char
+* @accept: char
+* Return:  pointer to the byte in s that matches one of the bytes
+*/
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+	char *p;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (accept[j] != '\0')
+		{
+			if (accept[j] == s[i])
+			{
+				p = s[i];
+				return (p);
+			}
+			j++;
+		}
+		i++;
+	}
+	return (0);
+
+}
