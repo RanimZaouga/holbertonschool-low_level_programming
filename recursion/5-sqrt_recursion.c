0f72544f85dbe7f1de9dfe7c44f5089fbb0572b9
RanimZaouga <ranim.zaougaa@gmail.com>
2023-07-06 12:27:44 +0100

    recursion

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,17 +1,32 @@
 #include "main.h"
 
 /**
-* _sqrt_recursion - returns natural square root of a number
-* @n: input
-* Return: Always 0.
+* sqrt_recursive - helper function for calculating square root recursively
+* @n: input number
+* @guess: initial guess for the square root
+* Return: square root of n
 */
-#include "main.h"
+int sqrt_recursive(int n, int guess)
+{
+	if (guess * guess == n)
+		return guess;
+	else if (guess * guess > n)
+		return -1;
+	else
+		return sqrt_recursive(n, guess + 1);
+}
 
+/**
+* _sqrt_recursion - returns the natural square root of a number
+* @n: input number
+* Return: square root of n
+*/
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
-	{
-		return (-1);
-	}
-	return (_sqrt_recursion(n, 1.0));
+		return -1;
+	else if (n == 0 || n == 1)
+		return n;
+	else
+		return sqrt_recursive(n, 1);
 }
