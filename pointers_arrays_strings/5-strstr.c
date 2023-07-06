ba351dc381e8ca57646ab35887b37bbfe08d9402
RanimZaouga <ranim.zaougaa@gmail.com>
2023-07-06 02:03:01 +0100

    fix error

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* _strstr - locates a substring
+* @haystack: char
+* @needle: char
+* Return:  pointer
+*/
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *h = haystack;
+		char *n = needle;
+		while (*h == *n && *n != '\0')
+		{
+			h++;
+			n++;
+		}
+		if (*n == '\0')
+		return (haystack);
+	}
+	return (0);
+}
