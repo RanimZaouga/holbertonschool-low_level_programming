0f089f7229a4e43949f44e171712d35158215b5e
RanimZaouga <ranim.zaougaa@gmail.com>
2023-07-06 11:12:05 +0100

    recursion

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* _print_rev_recursion - print a string in reverse
+* @s: input
+* Return: Always 0.
+*/
+void _print_rev_recursion(char *s)
+{
+    if (*str == '\0')
+        return;
+    _print_rev_recursion(s + 1);
+    _putchar(*s);        
+}
