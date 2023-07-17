c59731a1b891c1b8f38c3bbf4c80a10f8dee6850
RanimZaouga <ranim.zaougaa@gmail.com>
2023-07-17 11:43:04 +0100

    structures

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,10 +1,19 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - dog information
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: the name of the owner
+ *
+ * Description: dog struct
+ */
 
-struct dog {
-    char* name;
-    float age;
-    char* owner;
+struct dog 
+{
+	char *name;
+	float age;
+	char *owner;
 };
 
  #endif
