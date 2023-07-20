cd315dd75658d6cc7e0b9a44fdd02960f498fc6e
RanimZaouga <ranim.zaougaa@gmail.com>
2023-07-20 10:44:04 +0100

    function pointers

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -2,7 +2,7 @@
 #include "function_pointers.h"
 
 /**
- * print_name_as_is - prints a name as is
+ * print_name - prints a name
  * @name: name of the person
  * @f: function pointer
  * Return: Nothing.
@@ -12,5 +12,5 @@ void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
 		return;
-	f(name);	
+	f(name);
 }
