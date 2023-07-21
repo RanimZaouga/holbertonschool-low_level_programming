a48094185824ce1308ba9548f645c26972edd74f
RanimZaouga <ranim.zaougaa@gmail.com>
2023-07-21 19:26:03 +0100

    ptr functions

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -12,34 +12,33 @@ int main(int argc, char *argv[])
 {
 	int a, b;
 
+
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-
-	if (argv[2][0] != '+' && argv[2][0] != '-' && argv[2][0] != '*' && argv[2][0] != '/' && argv[2][0] != '%')
+	if (argv[2][0] != '+' && argv[2][0] != '-' && argv[2][0] != '*'
+			&& argv[2][0] != '/' && argv[2][0] != '%')
 	{
 		printf("Error\n");
 		exit(98);
 	}
-
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
+
 
 	if ((argv[2][0] == '/' || argv[2][0] == '%') && b == 0)
 	{
 		printf("Error\n");
 		exit(100);
-	}
 
-	int result = get_op_func(argv[2])(a, b);
-	if (result == -1) // Check if the operator is valid using the get_op_func function
-	{
-		printf("Error\n");
-		exit(99);
 	}
-
-	printf("%d\n", result);
+/*	if (get_op_func(argv[2]) == NULL)  */
+/*	{*/
+	/*	printf("Error\n");*/
+	/*	exit(99);*/
+/*	}*/
+	printf("%d\n", get_op_func(argv[2])(a, b));
 	return (0);
 }
