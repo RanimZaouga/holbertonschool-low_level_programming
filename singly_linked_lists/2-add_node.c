d2350a53700e5ca815fddb6f3a72cc351f5dddff
RanimZaouga <ranim.zaougaa@gmail.com>
2023-08-05 22:14:51 +0100

    print list

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -12,7 +12,7 @@ list_t *add_node(list_t **head, const char *str)
 	if (str == NULL)
 		return (NULL);
 
-	list_t *new_node = malloc(sizeof(list_t0));
+	list_t *new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
 
@@ -22,5 +22,4 @@ list_t *add_node(list_t **head, const char *str)
 		free(new_node);
 		return (NULL);
 	}
-	
 }
