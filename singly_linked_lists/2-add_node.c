2e7ac410bdedbb58ed7f598c7333872415ab67a5
RanimZaouga <ranim.zaougaa@gmail.com>
2023-08-05 22:32:10 +0100

    print list

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -11,23 +11,23 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *ptr;
 
-    if (str == NULL)
-        return (NULL);
+	if (str == NULL)
+		return (NULL);
 
-    ptr = malloc(sizeof(list_t));
-    if (ptr == NULL)
-        return (NULL);
+	ptr = malloc(sizeof(list_t));
+	if (ptr == NULL)
+		return (NULL);
 
-    ptr->str = strdup(str);
-    if (ptr->str == NULL)
-    {
-        free(ptr);
-        return (NULL);
-    }
+	ptr->str = strdup(str);
+	if (ptr->str == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
 
-    ptr->len = strlen(str);
-    ptr->next = *head;
-    *head = ptr;
+	ptr->len = strlen(str);
+	ptr->next = *head;
+	*head = ptr;
 
-    return (ptr);
+	return (ptr);
 }
