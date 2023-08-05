38f68e7c67c3336a689cd15eeacaf595eccf5d29
RanimZaouga <ranim.zaougaa@gmail.com>
2023-08-05 17:24:08 +0100

    print list

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+typedef struct list_node {
+	char *str;               // String data stored in the node
+	struct list_node *next;  // Pointer to the next node
+} list_t;
+
+size_t print_list(const list_t *h)
+{
+	const list_t *current = h;  // Start from the head of the list
+	size_t count = 0;           // Counter for the number of nodes
+
+	 while (current != NULL)
+	 {
+		if (current->str == NULL)
+		{
+			printf("[0] (nil)");  // Print "[0] (nil)" if the string is NULL
+		}
+		else 
+		{
+			size_t len = 0;
+			while (current->str[len] != '\0')
+			{
+				len++;  // Calculate the string length manually
+			}
+			printf("[%zu] %s", len, current->str); // Print the string length and the string
+		}
+
+		printf("\n");  // Print a new line after printing each node
+		count++;       // Increment the node count
+		current = current->next;  // Move to the next node
+	}
+
+	return count;  // Return the total number of nodes
+}
