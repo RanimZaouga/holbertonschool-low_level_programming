ee330d461f54167736821fada5d9a191ceb97c90
RanimZaouga <ranim.zaougaa@gmail.com>
2023-08-06 15:42:46 +0100

    print list

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * print_dlistint - function that print all the elements of a list
+ * @h: pointer to the head of the list
+ * Return: number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	dlistint_t i;
+
+	while (h)
+	{
+		i++;
+		printf("%d ", h->n)
+		h = h->next
+	}
+	return (i);
+}
---
+++ doubly_linked_lists/README.md
@@ -0,0 +1,1 @@
+readme doubli linked lists
