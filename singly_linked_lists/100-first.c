40c3e9fa941acd750862cd944a39da3335c29052
RanimZaouga <ranim.zaougaa@gmail.com>
2023-08-06 14:11:06 +0100

    print list

---
+++ singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * before_main - function that print string before the main functionis excuted
+ */
+void print_before_main(void) __attribute__((constructor));
+
+void print_before_main(void)
+{
+    printf("You're beat! and yet, you must allow,\n"
+           "I bore my house upon my back!\n");
+}
