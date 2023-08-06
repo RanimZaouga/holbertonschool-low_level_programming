f5bfd57e251851153d75fd1190c986541cbdece7
RanimZaouga <ranim.zaougaa@gmail.com>
2023-08-06 15:50:48 +0100

    print list

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -12,9 +12,8 @@ size_t print_dlistint(const dlistint_t *h)
 	while (current != NULL)
 	{
 		count++;
-		printf("%d ", current->n);
+		printf("%d\n", current->n);
 		current = current->next;
 	}
-	printf("\n");
 	return (count);
 }
