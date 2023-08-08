9256c255fe6ac4512a59336d3d799f5914696e23
RanimZaouga <ranim.zaougaa@gmail.com>
2023-08-08 09:52:47 +0100

    print list

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -9,17 +9,17 @@
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-    unsigned int count = 0;
-    dlistint_t *current = head;
+	unsigned int count = 0;
+	dlistint_t *current = head;
 
-    while (current != NULL)
-    {
-        if (count == index)
-            return current;
+	while (current != NULL)
+	{
+		if (count == index)
+			return (current);
 
-        current = current->next;
-        count++;
-    }
+		current = current->next;
+		count++;
+	}
 
-    return NULL;
+	return (NULL);
 }
