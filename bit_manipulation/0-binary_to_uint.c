eed71111a79e6b03023cee2d2fd8d746a8149baf
RanimZaouga <ranim.zaougaa@gmail.com>
2023-08-08 15:32:55 +0100

    bit manipulation

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+unsigned int binary_to_uint(const char *b) 
+{
+	if (b == NULL)
+		return (0);
+
+	unsigned int result = 0;
+	for (int i = 0; b[i] != '\0'; i++) {
+		char digit = b[i];
+		if (digit == '0' || digit == '1') {
+			result = (result << 1) + (digit - '0');
+		} else {
+			return (0); /* Invalid character found*/
+		}
+	}
+
+	return (result);
+}
