dc6b2f0c5ed40dcbc13355816fda5e7a5a73deb0
RanimZaouga <ranim.zaougaa@gmail.com>
2023-08-08 15:43:51 +0100

    print list

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -1,19 +1,20 @@
-#include <stdio.h>
+#include "main.h"
 
 unsigned int binary_to_uint(const char *b) 
 {
-	if (b == NULL)
-		return (0);
+    if (b == NULL)
+        return (0);
 
-	unsigned int result = 0;
-	for (int i = 0; b[i] != '\0'; i++) {
-		char digit = b[i];
-		if (digit == '0' || digit == '1') {
-			result = (result << 1) + (digit - '0');
-		} else {
-			return (0); /* Invalid character found*/
-		}
-	}
+    unsigned int result = 0; // Move the declaration here
+    int i; // Declare 'i' here
+    for (i = 0; b[i] != '\0'; i++) {
+        char digit = b[i];
+        if (digit == '0' || digit == '1') {
+            result = (result << 1) + (digit - '0');
+        } else {
+            return (0); /* Invalid character found*/
+        }
+    }
 
-	return (result);
+    return (result);
 }
