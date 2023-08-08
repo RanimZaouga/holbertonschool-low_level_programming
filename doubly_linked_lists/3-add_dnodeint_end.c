77a8277a157b6e8d9db0cb6097a5e14f5d1e6198
RanimZaouga <ranim.zaougaa@gmail.com>
2023-08-08 09:28:26 +0100

    print list

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -36,6 +36,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		/* Add the new node after the last node */
 		i->next = new_node;
 	}
-	return (new_node);
 	new_node->prev = i;
+	
+	return (new_node);
 }
