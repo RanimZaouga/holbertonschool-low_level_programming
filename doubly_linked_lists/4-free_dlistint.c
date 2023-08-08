0ed9046392880e4a9115a086bcffba587014c1b3
RanimZaouga <ranim.zaougaa@gmail.com>
2023-08-08 09:36:23 +0100

    print list

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_dlistint - frees a dlistint_t list
+ *
+ * @head: pointer to the head of the list
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *i;
+
+	while (head != NULL)
+	{
+		i = head;
+		head = head->next;
+		free(i->str);
+		free(i);
+	}
+
+}
