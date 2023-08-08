2f1a34370b6d5ebdb56e65aca7e3008bc8a03b82
RanimZaouga <ranim.zaougaa@gmail.com>
2023-08-08 15:45:08 +0100

    print list

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -5,9 +5,10 @@ unsigned int binary_to_uint(const char *b)
     if (b == NULL)
         return (0);
 
-    unsigned int result = 0; // Move the declaration here
-    int i; // Declare 'i' here
-    for (i = 0; b[i] != '\0'; i++) {
+    unsigned int result = 0;
+    int i;
+    for (i = 0; b[i] != '\0'; i++)
+	{
         char digit = b[i];
         if (digit == '0' || digit == '1') {
             result = (result << 1) + (digit - '0');
