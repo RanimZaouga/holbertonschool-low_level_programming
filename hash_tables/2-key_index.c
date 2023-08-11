e3d47a1c3353e555bd9518656d9075d1ad42a6d0
RanimZaouga <ranim.zaougaa@gmail.com>
2023-08-11 15:17:23 +0100

    hash tables

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -11,13 +11,5 @@
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-    unsigned long int hash;
-	int c;
-
-	hash = 5381;
-	while ((c = *str++))
-	{
-		hash = ((hash << 5) + hash) + c;
-	}
-	return (hash);
+	return (hash_djb2(key) % size);
 }
