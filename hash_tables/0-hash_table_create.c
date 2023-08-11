8919ad686bc1fbe7e339290e6dc5f3a1a6bcef08
RanimZaouga <ranim.zaougaa@gmail.com>
2023-08-11 15:08:23 +0100

    hash tables

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -9,24 +9,24 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *ht;  /* Declare a pointer to a hash table structure */
-    unsigned long int i;  /*Declare a variable for iteration*/
+	hash_table_t *ht;  /* Declare a pointer to a hash table structure */
+	unsigned long int i;  /*Declare a variable for iteration*/
 
-    /* Allocate memory for the hash table structure */
-    ht = malloc(sizeof(hash_table_t));
-    if (ht == NULL)  /* Check if memory allocation was successful */
-        return (NULL);
+	/* Allocate memory for the hash table structure */
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)  /* Check if memory allocation was successful */
+		return (NULL);
 
-    ht->size = size;  /* Set the size of the hash table */
+	ht->size = size;  /* Set the size of the hash table */
 
-    /* Allocate memory for the array of hash nodes */
-    ht->array = malloc(sizeof(hash_table_t *) * size);
-    if (ht == NULL)  /* Check if memory allocation was successful */
-        return (NULL);
+	/* Allocate memory for the array of hash nodes */
+	ht->array = malloc(sizeof(hash_table_t *) * size);
+	if (ht == NULL)  /* Check if memory allocation was successful */
+		return (NULL);
 
-    /* Initialize each element of the array to NULL */
-    for (i = 0; i < size; i++)
-        ht->array[i] = NULL;
+	/* Initialize each element of the array to NULL */
+	for (i = 0; i < size; i++)
+		ht->array[i] = NULL;
 
-    return (ht);  /* Return the pointer to the newly created hash table */
+	return (ht);  /* Return the pointer to the newly created hash table */
 }
