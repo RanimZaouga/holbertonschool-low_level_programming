7204228421ca7b5bb746efdbfff6807588d25608
RanimZaouga <ranim.zaougaa@gmail.com>
2023-09-23 20:54:57 +0100

    searching algos

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -7,6 +7,6 @@ int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 
 /* not main */
-void print_array(int *array, size_t inc, size_t size);
+int recursive_search(int *array, size_t size, int value)
 
 #endif
