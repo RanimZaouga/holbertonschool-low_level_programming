a27438635b1256cabd6720e6b0c607f0b580bab3
RanimZaouga <ranim.zaougaa@gmail.com>
2023-09-23 20:57:10 +0100

    searching algos

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -1,12 +1,10 @@
-#ifndef SEARCH_ALGOS
-#define SEARCH_ALGOS
+#ifndef _BINARY_SEARCH_
+#define _BINARY_SEARCH_
+
+#include <stdlib.h>
 #include <stdio.h>
 
-/* main functions */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
-
-/* not main */
-int recursive_search(int *array, size_t size, int value)
 
 #endif
